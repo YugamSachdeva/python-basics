47ba783634e9aad861c043c199f6bd110b99e488
Yugam Sachdeva <sachdevayugam55@gmail.com>
2026-02-02 17:25:59 +0530

    Add basic list operations example

---
+++ lists-tuples-sets/list_operations.py
@@ -0,0 +1,15 @@
+# List operations example
+
+lst = ["banana", "orange", "grapes", "chikoo"]
+
+# Access first and last element
+print(lst[0], lst[-1])
+
+# Add elements
+lst.append("mango")
+lst.insert(1, "pineapple")
+print(lst)
+
+# Remove element
+lst.remove("grapes")
+print(lst)
