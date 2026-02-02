9e0c96b23dba2c8cd1a2dc403b61d3ca988c5b4b
Yugam Sachdeva <sachdevayugam55@gmail.com>
2026-02-02 17:49:05 +0530

    Add math and datetime module examples

---
+++ modules/math_datetime.py
@@ -0,0 +1,10 @@
+# Python modules example (math & datetime)
+
+import math
+import datetime
+
+print("Square root of 16:", math.sqrt(16))
+print("Value of PI:", math.pi)
+
+current_time = datetime.datetime.now()
+print("Current date and time:", current_time)
