fa5fe3e5f8163514524fb2dc204a46aba7ff6fd9
Yugam Sachdeva <sachdevayugam55@gmail.com>
2026-02-02 17:32:02 +0530

    Add basic functions example

---
+++ functions/basic_functions.py
@@ -0,0 +1,13 @@
+# Basic functions example
+
+def square(n):
+    return n * n
+
+def even_odd(n):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+print(square(6))
+print(even_odd(9))
